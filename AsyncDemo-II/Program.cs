20ad7ddd9483e93e3d3442fb79e2c7bc29ed8dac
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-03-03 08:44:10 +0300

    Add project files.

---
+++ AsyncDemo-II/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            ProcessIAsync();
+            ProcessIIAsync();
+          
+   
+
+            Console.ReadLine();
+        }
+        //This is how me make our method async without calling Task task1 = Task.Run(ProcessI)
+        static async Task ProcessIAsync()
+        {
+
+           await Task.Run(() =>
+            {
+                Console.WriteLine($"Process I is running... Thread No : {Thread.CurrentThread.ManagedThreadId}");
+            });
+        }
+
+        static async Task ProcessIIAsync()
+        {
+            await Task.Run(() => 
+            {
+                Console.WriteLine($"Process II is running... Thread No : {Thread.CurrentThread.ManagedThreadId}");
+
+            });
+        }
+    }
+}
---
+++ AsyncDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Thread No : {Thread.CurrentThread.ManagedThreadId}");
+
+
+            //Task task1 = Task.Factory.StartNew(ProcessI);
+            //ProcessII();
+
+            //Task task1 = new Task(ProcessI);
+            //task1.Start();
+            //ProcessII();
+
+            //Task task1 = Task.Run(ProcessI);
+            //ProcessII();
+
+            //All above piece of code runs our Method as Async but always ProcessII runs first cause codes flow down very fast main thread doesnt wait until Task create new instance in the memory instead finish its thread then continue with its 
+
+            //Task task1 = Task.Run(() =>
+            //{
+            //    ProcessI();
+            //    ProcessII();
+            //});
+
+            //Above piece of code runs our Method as Async and create new Thread channel but First ProcessI runs until it finished then ProcessII runs
+
+            Task task1 = Task.Run(ProcessI);
+            task1.Wait();
+            ProcessII();
+
+
+
+            Console.ReadLine();
+        }
+
+        static void ProcessI()
+        {
+            Console.WriteLine("Process I is running...");
+            Console.WriteLine($"Thread No : {Thread.CurrentThread.ManagedThreadId}");
+
+        }
+
+        static void ProcessII()
+        {
+            Console.WriteLine("Process II is running...");
+            Console.WriteLine($"Thread No : {Thread.CurrentThread.ManagedThreadId}");
+
+        }
+
+    }
+}
